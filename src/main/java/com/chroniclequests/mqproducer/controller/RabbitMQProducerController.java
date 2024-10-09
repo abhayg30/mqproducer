667cabeec45ccbcd27969fcf41b3e6295f42f8f0
abhayg30 <abhaygupta.3003@gmail.com>
2024-10-10 10:02:04 +1100

    cross origin

--- src/main/java/com/chroniclequests/mqproducer/controller/RabbitMQProducerController.java
+++ src/main/java/com/chroniclequests/mqproducer/controller/RabbitMQProducerController.java
@@ -4,13 +4,11 @@ import com.chroniclequests.mqproducer.dto.RabbitMQDto;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/live-location", produces = MediaType.APPLICATION_JSON_VALUE)
+@CrossOrigin
 public class RabbitMQProducerController {
 
     private final RabbitTemplate rabbitTemplate;
